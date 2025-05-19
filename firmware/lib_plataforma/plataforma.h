aef6dec3ecdb6d5c90d3bc35f758fd7a64cbc3cc
diogo-gampe <diogomoraes09@gmail.com>
2025-05-19 10:40:26 -0300

    Update plataforma.h adicionado métodos de tara e calibração

--- firmware/lib_plataforma/plataforma.h
+++ firmware/lib_plataforma/plataforma.h
@@ -9,11 +9,16 @@
 #define plataforma_h
 
 
+
+//pinos PA10 -> RX
+//      PA09 -> TX
+
+//pinos de data e clock para hx711
 #define DATAPIN_1 PB12
 #define SCKPIN_1 PB13
 
-#define DATAPIN_2 0
-#define SCKPIN_2 1
+#define DATAPIN_2 PB10
+#define SCKPIN_2 PB11
 
 #define DATAPIN_3 2
 #define SCKPIN_3 3
@@ -27,10 +32,13 @@
 #define DATAPIN_6 8
 #define SCKPIN_6 9
 
-#define NUM_MEDICOES 5
+#define NUM_MEDICOES 2
+
+#define SIGNAL_TIMEOUT 400 //ms
+#define CALIB_TIMEOUT 2000 //ms
 
 //função para fazer extensão de sinal de 24 bits para 32 bits
-signed long convert24bitstoLong(signed long value24bits);
+int convert24bitstoLong(int value24bits);
 
 
 class Cel_Carga{
@@ -38,34 +46,45 @@ class Cel_Carga{
     private: 
       uint8_t _dataPin;
       uint8_t _sckPin;
+      
   
-      float offset;
-      float scale; 
-    
     public:
+
+        float tare;
+        float offset;
+        float scale; 
+        int lastDoutLowTime; 
+        uint8_t num_conversoes; 
+
         //método construtor
         Cel_Carga(uint8_t dataPin, uint8_t sckPin);
 
-        //método setar offset após calibração
-        void setOffset(float off);
+        // //método setar offset após calibração
+        // void setOffset(float off);
 
-        //método setar scale após calibração
-        void setScale(float scl);
+        // //método setar scale após calibração
+        // void setScale(float scl);
 
-        //método obter offset após calibração
-        float getOffset();
-        //método obter scale após calibração
-        float getScale();
+        // //método obter offset após calibração
+        // float getOffset();
+        // //método obter scale após calibração
+        // float getScale();
 
         //método para retornar pino clock
         float getSckPin();
         //método para retornar pino de dados
         float getDataPin();
         //método obter valor digital a partir de HX711
-        signed long readRaw();
+        int readRaw();
 
         //método para obter valor de força após conversão de valor digital
-        float getValue();
+        float getValue(float raw);
+
+        float doTare();
+        float doCalibration(float massa_g);
+
+        
+
   
   };
 
